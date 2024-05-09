cc077c5dbef99d288aadcb0a9a7414756a6e9580
John Truskov <truskov@bk.ru>
2024-05-09 13:27:29 +0500

    add checkbox remember_me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,14 +12,25 @@
                 </ul>
             </div>
         @endif
-        <form action="/login" method="post">
+        <form action="{{ route('login') }}" method="post">
             @csrf
-            <p>Нет аккаунта? <a href="/register">Зарегистрироваться</a> | Забыли пароль? <a href="{{ route('password.request') }}">Восстановить</a></p>
+            <div class="row justify-content-around mb-3">
+                <div class="col-5">
+                    Нет аккаунта? <a href="/register">Зарегистрироваться</a>
+                </div>
+                <div class="col-5">
+                    Забыли пароль? <a href="{{ route('password.request') }}">Восстановить</a>
+                </div>
+            </div>
             <div class="mb-3">
                 <input name="email" type="email" class="form-control" placeholder="E-mail" required autofocus>
             </div>
             <div class="mb-3">
                 <input name="password" type="password" class="form-control" placeholder="Пароль" required>
+            </div>
+            <div class="mb-3">
+                <input id="remember_me" name="remember" type="checkbox" class="form-check-input">
+                <label for="remember_me" class="form-check-label">Запомнить меня</label>
             </div>
             <div class="mb-3">
                 <input type="submit" class="form-control btn btn-primary" value="Войти">
